7c8a86473ba5d47e7764b8086d38e85cf318176d
joniz <jaej96@gmail.com>
2017-02-10 11:53:23 +0100

    Uppdaterat

---
+++ server-side/server-side/Controllers/AuthorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace server_side.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        // GET: Authors
+        public ActionResult authors()
+        {
+            return View("Authors");
+        }
+    }
+}
---
+++ server-side/server-side/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(server_side.Startup))]
+namespace server_side
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
