88b38c8428ecc020294f62b384aa9267d85b8011
joniz <jaej96@gmail.com>
2017-02-10 11:01:20 +0100

    Första commiten

---
+++ WebApplication1/WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using service.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BooksController : Controller
+    {
+        List<Books> _bookList = new List<Books>();
+        
+        
+        // GET: Books
+        public ActionResult books()
+        {
+            return View("books");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/logInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class logInController : Controller
+    {
+        // GET: logIn
+        public ActionResult logIn()
+        {
+            return View("logIn");
+        }
+    }
+}
