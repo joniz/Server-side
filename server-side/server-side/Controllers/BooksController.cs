041321682b517db34f3fa1b9a9cc63f551d1dda6
joniz <jaej96@gmail.com>
2017-02-13 16:03:45 +0100

    Lite mockups och annan skit

--- server-side/server-side/Controllers/BooksController.cs
+++ server-side/server-side/Controllers/BooksController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using service.Models;
 namespace server_side.Controllers
 {
     public class BooksController : Controller
