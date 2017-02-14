de4ca369b3c78b22198fcca5d4b7ba4c246cf322
joniz <jaej96@gmail.com>
2017-02-14 15:31:01 +0100

    Lite grejer

---
+++ server-side/service/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using service.Mockups;
+namespace service.Models
+{
+   public class Author
+    {
+       public int _aId { get; set; }
+       public string _firstName { get; set; }
+       public string _lastName { get; set; }
+       public int _birthYear { get; set; }
+
+
+        public static List<Author> getAuthorList()
+        {
+            return Mockups.authorMockup.authorList;
+        }
+
+    }
+}
